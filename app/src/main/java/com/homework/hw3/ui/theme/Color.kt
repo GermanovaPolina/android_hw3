d095e498cadc79b952615fe2622e21bb90fb2366
Roman Chechevatov <chechevatov.r@gmail.com>
2024-12-22 18:00:05 +0300

    Add colors

--- app/src/main/java/com/homework/hw3/ui/theme/Color.kt
+++ app/src/main/java/com/homework/hw3/ui/theme/Color.kt
@@ -2,6 +2,9 @@ package com.homework.hw3.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
+val White = Color(0xFFFFFFFF)
+val Black = Color(0xFF000000)
 val SecondaryColor = Color(0xFFD3BEE3)
 val BaseColor = Color(0xFFF0F0F0)
 val SelectedColor = Color(0xFFC3ADD4)
+val PriceFontColor = Color(0xFF4F1ABC)
